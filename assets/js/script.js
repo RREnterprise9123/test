a77bc1ac53f2c3db475dfb1da7f57a2614eecfce
RREnterprise9123 <95470430+RREnterprise9123@users.noreply.github.com>
2021-12-03 14:07:40 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+console.log("This Website RR Enterprise Is Made By Nakshatra Ranjan Saha (OCEAN OF ANYTHING)")
+function formSubmited() {
+    submitted = true;
+    document.getElementById("g7567fggdebrfh").click();
+    document.getElementById("resetInput").click();
+};
